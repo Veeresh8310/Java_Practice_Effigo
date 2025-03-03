29be9b73001aa2e231ab24f1b5dd0ffa393d507a
Veeresh Akki <152872684+Veeresh8310@users.noreply.github.com>
2025-03-03 16:34:32 +0530

    Add files via upload

---
+++ QA Training/src/Automation_Testing/Testing_1/src/test/java/Thursday_work/Auto_Suggestion.java
@@ -0,0 +1,37 @@
+package Thursday_work;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Auto_Suggestion {
+	
+	public static void main(String[] args)
+	{
+		
+		 
+	     ChromeDriver driver=new ChromeDriver();
+	     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(300));
+	     driver.get("https://www.google.com");
+	     
+	    driver.findElement(By.className("gLFyf")).sendKeys("java");
+	    List<WebElement> suggestion=driver.findElements(By.xpath("//div[@class='OBMEnb']//li"));
+	    System.out.println(suggestion.size());
+	    for(WebElement s:suggestion)
+	    {
+	    	String su=s.getText();
+	    	System.out.println(su);
+	    }
+	     	    
+	     driver.close();
+	     
+	}
+	
+	
+
+}
